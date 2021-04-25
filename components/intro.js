634e497c2cc0fac1dc4b487747edc7e2a72886ba
Emma Westbrook <emmalwestbrook@gmail.com>
2021-04-25 18:47:04 -0500

    updated og

--- components/intro.js
+++ components/intro.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mx-10 mt-16 mb-16 md:mb-12">
+    <section className="flex-col md:flex-row flex items-center md:justify-between md:mx-10 mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-7xl font-tactile tracking-tighter leading-tight md:pr-8">
         Emma Westbrook
       </h1>
